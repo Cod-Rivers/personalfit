5bfa0b78e5ffcf4bc050ca5c91024dbe18ededd0
riverkirasamura <riversonsmorais@hotmail.com>
2025-12-18 16:38:56 -0300

    fix: corrige captura de dados da anamnese

--- src/components/organism/QuestionsRenderer/index.tsx
+++ src/components/organism/QuestionsRenderer/index.tsx
@@ -3,26 +3,29 @@ import React, { FC, useEffect, useState } from 'react';
 import { IQuestionProps, IquestionsRendererProps } from './interface';
 import Timeline from '@/components/molecules/Timeline';
 
-
 // import { Container } from './styles';
 
-const QuestionsRenderer: FC<IquestionsRendererProps> = ({ questions,  submitQuestions }) => {
+const QuestionsRenderer: FC<IquestionsRendererProps> = ({
+    questions,
+    submitQuestions,
+}) => {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
     const [answers, setAnswers] = useState<{ [key: string]: string }>({});
 
-    const currentQuestion: IQuestionProps = questions[currentQuestionIndex ?? 0];
+    const currentQuestion: IQuestionProps =
+        questions[currentQuestionIndex ?? 0];
     const isLastQuestion = currentQuestionIndex === questions.length - 1;
 
-    const handleRadioChange = (answer_id: string) => {
+    const handleRadioChange = (question_id: string, answer_id: string) => {
         setAnswers({
             ...answers,
-            [currentQuestion.id]: answer_id,
+            [question_id]: answer_id,
         });
     };
 
     const handlePreviousQuestion = () => {
-        setCurrentQuestionIndex(currentQuestionIndex - 1)
-    }
+        setCurrentQuestionIndex(currentQuestionIndex - 1);
+    };
 
     const handleNextQuestion = () => {
         if (!isLastQuestion) {
@@ -37,13 +40,30 @@ const QuestionsRenderer: FC<IquestionsRendererProps> = ({ questions,  submitQues
                 <h1>{currentQuestion.text}</h1>
             </div>
             <div className="">
-                <div className='d-flex flex-column gap-3 my-3 mb-4'>
+                <div className="d-flex flex-column gap-3 my-3 mb-4">
                     {currentQuestion.options.map((option) => {
                         return (
-                            <button key={option.answer_id} className='p-3 bg-white text-black rounded' style={{
-                                border: answers[option.question_id] === option.answer_id ? '3px solid var(--color-gold)' : '3px solid transparent'
-                            }} onClick={() => handleRadioChange(option.answer_id)}>
-                                <label htmlFor={option.question_id} className='fw-fold d-flex align-items-center'>
+                            <button
+                                key={option.answer_id}
+                                className="p-3 bg-white text-black rounded"
+                                style={{
+                                    border:
+                                        answers[option.question_id] ===
+                                        option.answer_id
+                                            ? '3px solid var(--color-gold)'
+                                            : '3px solid transparent',
+                                }}
+                                onClick={() =>
+                                    handleRadioChange(
+                                        option.question_id,
+                                        option.answer_id,
+                                    )
+                                }
+                            >
+                                <label
+                                    htmlFor={option.question_id}
+                                    className="fw-fold d-flex align-items-center"
+                                >
                                     {option.text}
                                 </label>
                             </button>
@@ -52,18 +72,24 @@ const QuestionsRenderer: FC<IquestionsRendererProps> = ({ questions,  submitQues
                 </div>
                 <div className="d-flex justify-content-between">
                     <button
-                        className='btn btn-gold'
+                        className="btn btn-gold"
                         onClick={handlePreviousQuestion}
                         disabled={currentQuestionIndex === 0}
                     >
                         Anterior
                     </button>
-                    <button className='btn btn-gold' onClick={handleNextQuestion}>
+                    <button
+                        className="btn btn-gold"
+                        onClick={handleNextQuestion}
+                    >
                         {isLastQuestion ? 'Finalizar' : 'Próxima'}
                     </button>
                 </div>
                 <div className="w-100 mt-3">
-                    <Timeline currentStep={currentQuestionIndex} totalSteps={questions.length} />
+                    <Timeline
+                        currentStep={currentQuestionIndex}
+                        totalSteps={questions.length}
+                    />
                 </div>
             </div>
         </div>
